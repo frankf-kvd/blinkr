b15bb08ad26da52dc5536053186127665b815fe5
frankf-kvd <119434731+frankf-kvd@users.noreply.github.com>
2022-11-29 18:28:25 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,10 @@
 let pomoc = 0
 input.onButtonPressed(Button.A, function () {
+    for (let n = 0; n <= 4; n++) {
+        for (let k = 0; k <= 4; k++) {
+            led.unplot(n, k)
+        }
+    }
     for (let index = 0; index < 3; index++) {
         for (let i = 0; i <= 4; i++) {
             pomoc = 4 - i
@@ -12,18 +17,29 @@ input.onButtonPressed(Button.A, function () {
         control.waitMicros(100000)
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . # # # .
+        # # # # #
+        . # # # .
+        `)
+})
 input.onButtonPressed(Button.B, function () {
+    for (let n = 0; n <= 4; n++) {
+        for (let k = 0; k <= 4; k++) {
+            led.unplot(n, k)
+        }
+    }
     for (let index = 0; index < 3; index++) {
-        for (let i = 0; i <= 4; i++) {
-            led.plot(i, 2)
+        for (let m = 0; m <= 4; m++) {
+            led.plot(m, 2)
             control.waitMicros(100000)
         }
-        for (let k = 0; k <= 4; k++) {
-            led.unplot(k, 2)
+        for (let n = 0; n <= 4; n++) {
+            led.unplot(n, 2)
         }
         control.waitMicros(100000)
     }
-})
-basic.forever(function () {
-	
 })
